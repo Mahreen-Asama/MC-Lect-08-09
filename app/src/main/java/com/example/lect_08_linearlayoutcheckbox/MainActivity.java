0098e8fa98a3427eb967c1d96ddedc162fb6944e
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-05 10:11:55 +0500

    icon updated

--- app/src/main/java/com/example/lect_08_linearlayoutcheckbox/MainActivity.java
+++ app/src/main/java/com/example/lect_08_linearlayoutcheckbox/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.lect_08_linearlayoutcheckbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    CheckBox done,pending,read;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b=findViewById(R.id.button6);
+
+        done=findViewById(R.id.done);
+        pending=findViewById(R.id.pending);
+        read=findViewById(R.id.read);
+
+        done.setOnClickListener(this);
+        pending.setOnClickListener(this);
+        read.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.done:
+                if(done.isChecked())
+                    b.setText("done checked");
+                break;
+            case R.id.pending:
+                if(pending.isChecked())
+                    b.setText("pending checked");
+                break;
+            case R.id.read:
+                if(read.isChecked())
+                    b.setText("read checked");
+                break;
+        }
     }
 }
